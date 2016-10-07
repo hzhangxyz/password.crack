027726979a64d7b3561e027a06d98f029ccbb11a
trumpet <zh19970205@126.com>
2016-10-07 08:24:43 -0400

    update

--- src/f3.py
+++ src/f3.py
@@ -21,9 +21,23 @@ else:
 hash_position = [i*hash_length for i in hash_slices]
 dict_position = [i*dict_legnth for i in dict_slices]
 
-#get the file based on slice
-
 this_hash = "this.hash"
 this_dict = "this.dict"
 
-os.system("./hashcat -m %d %s %s"%(hash_id,this_hash,this_dict))
+hash_file_point = open(hash_file,"r")
+dict_file_point = open(dict_file,"r")
+hash_file_point.seek(hash_position[0])
+dict_file_point.seek(dict_position[0])
+
+this_hash_point = open(this_hash,"w")
+this_dict_point = open(this_dict,"w")
+
+this_hash_point.write(hash_file_point.read(hash_position[1]-hash_position[0]))
+this_dict_point.write(dict_file_point.read(dict_position[1]-dict_position[0]))
+
+hash_file_point.close()
+dict_file_point.close()
+this_hash_point.close()
+this_dict_point.close()
+
+os.system("./hashcat -a 0 -m %d %s %s"%(hash_id,this_hash,this_dict))
