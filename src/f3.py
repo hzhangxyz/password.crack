5330d94d050dcc31c351ddda407401bd4531f3a5
trumpet <zh19970205@126.com>
2016-10-07 09:16:26 -0400

    update

--- src/f3.py
+++ src/f3.py
@@ -12,16 +12,16 @@ ans_file   = sys.argv[6]
 hash_slices = map(int,hash_slice.split(":"))
 dict_slices = map(int,dict_slice.split(":"))
 
-dict_length = 34
+dict_length = 36
 
 if hash_type[0] == "m":
     hash_id     = 500
-    hash_length = 35
+    hash_length = 34
 else:
     hash_id     = 3200
-    hash_length = 61
+    hash_length = 60
 
-hash_ans_l = hash_length + dict_length + 1 # +1 -> :
+hash_ans_l = hash_length + dict_length + 1
 
 this_hash = "this.hash"
 this_dict = "this.dict"
@@ -34,15 +34,16 @@ dict_file_point = open(dict_file,"r")
 this_hash_point = open(this_hash,"w")
 this_dict_point = open(this_dict,"w")
 
-for i in range(hash_slices[0]):hash_file_point.readline()
+hash_file_point.seek(hash_slices[0]*(hash_length+1))
+#dict_file_point.seek(dict_slices[0]*(dict_length+1))
 for i in range(dict_slices[0]):dict_file_point.readline()
 
-ans_file_point.seek(hash_ans_l*hash_slices[0]+hash_length)
+ans_file_point.seek((1+hash_ans_l)*hash_slices[0]+hash_length+1)
 for i in range(hash_slices[1]-hash_slices[0]):
     to_write = hash_file_point.readline()
-    if ans_file_point.read(1)!="\n":
+    if ans_file_point.read(1)==":":
         this_hash_point.write(to_write)
-    ans_file_point.seek(hash_ans_l-1,1)
+    ans_file_point.seek(hash_ans_l,1)
 for i in range(dict_slices[1]-dict_slices[0]):this_dict_point.write(dict_file_point.readline())
 
 hash_file_point.close()
