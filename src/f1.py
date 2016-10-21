9122f164b1903dd75031b0f5a52a962a5cfa31d1
trumpet <zh19970205@126.com>
2016-10-22 02:38:42 +0800

    update

--- src/f1.py
+++ src/f1.py
@@ -1,20 +1,42 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
 
-# Server
+port_num = sys.argv[1]
+lock_num = sys.argv[2]
 
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 5320))
-s.listen(10)
-while True:
-    sock, addr = s.accept()
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
-def tcplink(sock, addr):
-    sock.send(b'Welcome!')
-    while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        if not data or data.decode('utf-8') == 'exit':
-            break
-        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
-    sock.close()
+is_file = re.compile("^[tlpcb]:\d+:\d+:\d+:\d+$").match
+is_tocr = re.compile("^t:\d+:\d+:\d+:\d+$").match
+is_lock = re.compile("^l:\d+:\d+:\d+:\d+$").match
+
+get_min = lambda x:int(x.split(":")[1])
+
+def init():
+    data = os.listdir("pool")
+    data.sort(key=get_min)
+    for i in data[:lock_num]:
+        os.rename("pool/%s"i,"pool/l%s"i[1:])
+
+def query_one():
+    data = os.listdir("pool")
+    lock = filter(is_lock,data)
+    tocr = filter(is_tocr,data)
+    ltop = min(lock,key=get_min)
+    ttol = min(tocr,key=get_min)
+    os.rename("pool/%s"%ttol,"pool/l%s"%ttol[1:])
+    os.rename("pool/%s"%ltop,"pool/p%s"%ltop[1:])
+    return ltop
+
+def application(environ, start_response):
+    query = environ["QUERY_STRING"]
+    if query == "query":
+        return query_one()
+    else:
+        os.rename("pool/p%s"%query[1:],"pool/%s"query)
+        return ""
+
+init()
+from wsgiref.simple_server import make_server
+httpd = make_server('', int(port_num), application)
+httpd.serve_forever()
--- src/pool/README.md
+++ src/pool/README.md
@@ -2,4 +2,6 @@ status:order:start:end
 
 t:to crack
 l:lock to change
+p:crack pending
 c:completed
+b:crack break
