3a82e57d0dd3403491c973db05203668259c0633
trumpet <zh19970205@126.com>
2016-11-09 18:41:59 +0800

    update

--- server/md5_pool/f.py
+++ server/md5_pool/f.py
@@ -1,7 +1,1 @@
-import os
-def copy(intf,startid,beginner,ender,num):
-    length = (ender - beginner)
-    for i in range(num):
-        n = "t:%d:%d:%d"%((i+startid),(beginner + length*i/num),(beginner + length*(i+1)/num))
-        os.system("ln -s %s %s"%(intf,n))
-
+../../runs/copier.py
