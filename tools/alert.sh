678726b790756c5df15e8593c210f564ee8975b8
trumpet <zh19970205@126.com>
2016-11-09 15:25:28 +0800

    update

--- tools/alert.sh
+++ tools/alert.sh
@@ -1,3 +1,8 @@
 kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`
 kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`
-pfornode killall -9 hashcat
+ssh node1 killall -9 hashcat &
+ssh node2 killall -9 hashcat &
+ssh node3 killall -9 hashcat &
+ssh node4 killall -9 hashcat &
+ssh node5 killall -9 hashcat &
+ssh node6 killall -9 hashcat &
