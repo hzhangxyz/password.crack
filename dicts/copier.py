325f72d3337dbfbfe1d2e5dcb7e9e493d71479a7
trumpet <zh19970205@126.com>
2016-11-09 15:23:45 +0800

    update

---
+++ dicts/copier.py
@@ -0,0 +1,7 @@
+import os
+def copy(intf,startid,beginner,ender,num):
+    length = (ender - beginner)
+    for i in range(num):
+        n = "t:%d:%d:%d"%((i+startid),(beginner + length*i/num),(beginner + length*(i+1)/num))
+        os.system("ln -s %s %s"%(intf,n))
+
