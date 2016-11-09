10837772b438a720256c81256ec684b38c667231
trumpet <zh19970205@126.com>
2016-11-09 15:26:36 +0800

    update

--- tools/alert.sh
+++ tools/alert.sh
@@ -1,5 +1,5 @@
-kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`
-kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`
+kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'`
+kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'`
 ssh node1 killall -9 hashcat &
 ssh node2 killall -9 hashcat &
 ssh node3 killall -9 hashcat &
