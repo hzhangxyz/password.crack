d896c4aae9ed1424262aa00324515f823efaedde
trumpet <zh19970205@126.com>
2016-11-13 23:32:47 +0800

    update

--- runs/killer.py
+++ runs/killer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 VERYDANGER = 2950
+VERYDANGER = 1100
 
 from socketIO_client import SocketIO, LoggingNamespace
 from subprocess import check_output
@@ -19,22 +20,24 @@ socketIO.on('power', p)
 def power_now():
     socketIO.wait(seconds=1)
     global m
-    return float(m)*22.5
+    return sum(m)
 
 def main():
     n = power_now()
     if flag:
         if n > VERYDANGER:
-            os.system("kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`")
-            os.system("kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'`")
+            killer = "kill -2 `ps aux | grep auto.py | grep -v grep | awk '{printf $2}'` &"
+            print killer
+            os.system(killer)
+            os.system(killer)
+            os.system("ssh node1 killall -9 hashcat &")
             os.system("ssh node2 killall -9 hashcat &")
-            os.system("ssh node3 killall -9 hashcat &")
-        a = len(check_output("ssh node2 nvidia-smi | awk '/hashcat/{printf $2\" \"}'",shell=True).split())
-        b = len(check_output("ssh node3 nvidia-smi | awk '/hashcat/{printf $2\" \"}'",shell=True).split())
-        print "%f\t%d\t%d"%(n,a,b)
+        a = len(check_output("ssh node1 nvidia-smi | awk '/hashcat/{printf $2\" \"}'",shell=True).split())
+        b = len(check_output("ssh node2 nvidia-smi | awk '/hashcat/{printf $2\" \"}'",shell=True).split())
+        print "%s\t%d\t%d"%(n,a,b)
     else:
         with open(argv[1],"a") as f:
-            f.write("%s %f\n"%(check_output("date +%s",shell=True)[:-1], n))
+            f.write("%s %s\n"%(check_output("date +%s",shell=True)[:-1], n))
 
 while True:
     main()
