f9583747dc7052f9ce6383762a360f096bedc190
trumpet <zh19970205@126.com>
2016-11-15 01:11:01 +0800

    update

--- tools/alert.sh
+++ tools/alert.sh
@@ -1,8 +1,6 @@
-kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'`
-kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'`
+kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'` &
+kill -2 `ps aux | grep starter.py | grep -v grep | awk '{printf $2}'` &
 ssh node1 killall -9 hashcat &
 ssh node2 killall -9 hashcat &
 ssh node3 killall -9 hashcat &
 ssh node4 killall -9 hashcat &
-ssh node5 killall -9 hashcat &
-ssh node6 killall -9 hashcat &
